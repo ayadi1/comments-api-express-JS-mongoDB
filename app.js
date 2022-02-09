6c8d0814632f90cefb6ad64b00b29f12e96bcc4a
ayadi1 <ayadiossama44@gmail.com>
2022-02-09 22:36:48 +0100

    add helmet package to secure api

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require('helmet')
 const CommentRouter = require("./routers/CommentRouter");
 const errorHandler = require("./error/errorHandler");
 const db = require("./db");
 const app = express();
+
+app.use(helmet())
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
